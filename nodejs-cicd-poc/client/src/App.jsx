9c2d0497b29c5f8210db11b1c6a6b0d2f051c085
Ayush Pandey <ayushpandey.ayush8737@gmail.com>
2026-04-28 00:17:58 +0530

    Initial CI/CD Node.js React app

---
+++ nodejs-cicd-poc/client/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="container">
+      <div className="card">
+        <h1>🚀 CI/CD Deployment</h1>
+        <p>
+          React App deployed automatically
+          <br />
+          using AWS CodePipeline
+        </p>
+        <button>Success ✅</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
